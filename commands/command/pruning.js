2dac1ec14e6d06d6ffd0ea84a5f056debcec56c7
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-09-11 15:36:26 +0700

    Create pruning.js

---
+++ commands/command/pruning.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "pruning",
+  cooldown: "3s",
+  code: `$description[$replaceText[$replaceText[$checkCondition[$pruneMusic==true];true;Pruning now **on**];false;Pruning now **off**]]
+$color[$getVar[color]]
+$addTimestamp
+$setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
+$onlyIf[$queueLength!=0;$getVar[errorqueue]]
+$cooldown[$commandInfo[pruning;cooldown];Please wait **%time%** before using again.]
+$onlyIf[$replaceText[$replaceText[$checkCondition[$getServerVar[userid]==default];true;$authorID];false;$getServerVar[userid]]==$authorID;{title:❌ You cant use this command} {color:$getVar[color]}]
+$onlyIf[$voiceID!=;$getVar[errorjoin]]
+$suppressErrors`
+}
