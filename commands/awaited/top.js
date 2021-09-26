0f2194b64d23bd92fe1b56992a2b221a2e41d0b3
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-09-26 22:51:47 +0700

    Update top.js

--- commands/awaited/top.js
+++ commands/awaited/top.js
@@ -2,7 +2,7 @@ module.exports = {
 type: "awaitedCommand",
 name: "top",
 code: `$deletecommand
-$title[Top Playing Song]
+$title[Top Playing Song - $numberSeparator[$charCount[$globalUserLeaderboard[userused;asc;\`) {top} {username} - {value}\`]]]]
 $description[$globalUserLeaderboard[userused;asc;\`) {top} {username} - {value}\`]]
 $color[$getVar[color]]
 $addTimestamp`
