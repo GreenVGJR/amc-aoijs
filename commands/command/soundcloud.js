f4197b6a127686fc176dc9a5a7e6b1c6753885e8
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-08 23:03:34 +0700

    Update soundcloud.js

--- commands/command/soundcloud.js
+++ commands/command/soundcloud.js
@@ -3,6 +3,9 @@ module.exports = {
   aliases: ["sc"],
   cooldown: "3s",
   code: `$if[$queueLength<1]
+$deleteMessage[$get[id]]
+$wait[3s]
+$editMessage[$get[id];{author:Starting Playing} {title:$get[song]} {color:$getVar[color]} {timestamp}]
 $else
 $author[Added to queue;$getVar[customemoji1]
 $title[$songInfo[title;$sub[$queueLength;1]];$songInfo[url;$sub[$queueLength;1]]]
@@ -17,6 +20,9 @@ $textSplit[$songInfo[duration;$sub[$queueLength;1]]; ]
 $endif
 $let[song;$playSoundcloud[$message;$getVar[clientidsoundcloud];$replaceText[$replaceText[$replaceText[$getGlobalUserVar[247];0;0s];1;120s];2;7d];yes;$replaceText[$replaceText[$replaceText[$getGlobalUserVar[247];0;yes];1;yes];2;no];No result.]]
 $joinVC[$voiceID]
+$if[$queueLength<1]
+$let[id;$sendMessage[{title:Starting Playing} {author:Loading..:$getVar[loademoji]} {color:$getVar[color]} {timestamp};yes]]
+$endif
 $botTyping
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
 $onlyIf[$replaceText[$replaceText[$checkCondition[$getServerVar[userid]==default];true;$authorID];false;$getServerVar[userid]]==$authorID;{title:❌ You cant use this command} {color:$getVar[color]}]
