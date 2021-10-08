a68bde078a81c3501fbbad2b9bd766f9d63af186
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-08 23:05:43 +0700

    Update ping.js

--- commands/command/ping.js
+++ commands/command/ping.js
@@ -6,7 +6,6 @@ Websocket Ping   : $numberSeparator[$ping]ms
 API       Ping   : $numberSeparator[$botPing]ms
 Database  Ping   : $numberSeparator[$dbPing]ms
 Message   Ping   : $executionTimems
-Shard     Ping   : $numberSeparator[$sub[$dateStamp;$get[stamp]]]ms $wait[$dbping] $let[stamp;$dateStamp]
 Average   Ping   : $numberSeparator[$truncate[$divide[$sum[$ping;$botPing;$dbPing];3]]]ms
 \`\`\`\
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
