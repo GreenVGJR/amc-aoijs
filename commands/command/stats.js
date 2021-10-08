2dd9dc8f1b1d50176e751af5f7a4b97a803976de
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-08 23:07:43 +0700

    Update stats.js

--- commands/command/stats.js
+++ commands/command/stats.js
@@ -3,15 +3,19 @@ module.exports = {
   cooldown: "3s",
   code: `$color[$getVar[color]]
 $addField[Size Database;> $cropText[$fileSize[$getVar[database];kb];5]KB;yes]
-$addField[Size Server;> $cropText[$numberSeparator[$multi[$get[sizeserver];8];.];5]KB;yes]
 $addField[Size Code;> $cropText[$fileSize[$getVar[file];kb];5]KB;yes]
 $addField[Command;> $numberSeparator[$commandsCount];yes]
 $addField[Server;> $numberSeparator[$serverCount];yes]
 $addField[Members;> $numberSeparator[$allMembersCount];yes]
 $addField[RAM Left;> $cropText[$divide[$sub[$maxRam;$ram];1024];4]GB;yes]
-$addField[RAM;> $cropText[$divide[$ram;1024];4]GB;yes]
+$addField[RAM;> $cropText[$divide[$ram;1024];4]GB
+> (Used)
+> $truncate[$cropText[$djsEval[process.memoryUsage().heapTotal / 1024 / 1024;yes];4]]MB
+> (Virtual Used)
+> $truncate[$cropText[$djsEval[process.memoryUsage().external / 1024 / 1024;yes];4]]MB
+> (External);yes]
 $addField[CPU;> $cropText[$cpu;4]%;yes]
-$addField[Is Deafen/Mute;> $replaceText[$isDeafened[$clientID];null;false] / $replaceText[$isMuted[$clientID];null;false];yes]
+$addField[Is Deafen/Mute;> $replaceText[$isDeafened[$clientID];null;false]-$replaceText[$isSelfDeafened[$clientID];null;false] / $replaceText[$isMuted[$clientID];null;false]-$replaceText[$isSelfMuted[$clientID];null;false];yes]
 $addField[Is Playing;> $checkCondition[$queueLength!=0];yes]
 $addField[Is Connect;> $checkCondition[$voiceID[$clientID]!=];yes]
 $addField[API Ping;> $numberSeparator[$botPing]ms;yes]
@@ -23,7 +27,6 @@ $addField[Uptime;> $client[readytimestamp];yes]
 $footer[Ver. $packageVersion ($nodeVersion);$userAvatar[$authorID;512]]
 $thumbnail[$userAvatar[$clientID]]
 $addTimestamp
-$let[sizeserver;$charCount[$serverNames[]]]
 $cacheMembers
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
 $cooldown[$commandInfo[stats;cooldown];Please wait **%time%** before using again.]`
