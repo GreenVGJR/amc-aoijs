5a24bfe80a0c91d2cea3f7cf0008dd67a1ed02b2
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:04:36 +0700

    Update ping.js

--- commands/command/ping.js
+++ commands/command/ping.js
@@ -1,7 +1,6 @@
 module.exports = {
   name: "ping",
-  cooldown: "3s",
-  code: `\`\`\`kt
+  code: `\`\`\`
 Websocket Ping   : $numberSeparator[$ping]ms
 API       Ping   : $numberSeparator[$botPing]ms
 Database  Ping   : $numberSeparator[$dbPing]ms
@@ -9,5 +8,5 @@ Message   Ping   : $executionTimems
 Average   Ping   : $numberSeparator[$truncate[$divide[$sum[$ping;$botPing;$dbPing];3]]]ms
 \`\`\`\
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
-$cooldown[$commandInfo[ping;cooldown];Please wait **%time%** before using again.]`
+$cooldown[3s;Please wait **%time%** before using again.]`
 }
