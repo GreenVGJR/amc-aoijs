3fd457931e06bff49b2fbf2dc6df04b47e3d916a
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:13:05 +0700

    Update remove.js

--- commands/command/remove.js
+++ commands/command/remove.js
@@ -1,7 +1,6 @@
 module.exports = {
   name: "remove",
   aliases: ["r"],
-  cooldown: "3s",
   code: `$moveSong[$sum[$message[1];1];]
 $title[$replaceText[$getVar[remove];{d-amount};$replaceText[$message[1];-;]]]
 $color[$getVar[color]]
@@ -10,7 +9,7 @@ $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
 $onlyIf[$message[1]<=$sub[$queueLength;1];Only have **$queueLength song**.]
 $onlyIf[$message[1]>1;You cant remove at first song.]
 $onlyIf[$isNumber[$message[1]]!=false;Must number!]
-$cooldown[$commandInfo[remove;cooldown];Please wait **%time%** before using again.]
+$cooldown[3s;Please wait **%time%** before using again.]
 $argsCheck[1;Usage: \`remove (numnber song on queue)\`]
 $onlyIf[$queueLength!=0;$getVar[errorqueue]]
 $onlyIf[$replaceText[$replaceText[$checkCondition[$getServerVar[userid]==default];true;$authorID];false;$getServerVar[userid]]==$authorID;{title:❌ You cant use this command} {color:$getVar[color]}]
