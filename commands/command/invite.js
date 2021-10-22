209befc08297be36587d17d97fbe50723e46701b
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:00:50 +0700

    Update invite.js

--- commands/command/invite.js
+++ commands/command/invite.js
@@ -1,8 +1,7 @@
 module.exports = {
 name: "invite",
-cooldown: "3s",
 code: `$replaceText[$getBotInvite;permissions=0;permissions=$getVar[permission]]
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
-$onlyIf[$client[ispublic]!=false;Your bot was private!]
-$cooldown[$commandInfo[invite;cooldown];Please wait **%time%** before using again.]`
+$onlyIf[$client[ispublic]!=false;Your bot was private]
+$cooldown[3s;Please wait **%time%** before using again.]`
 }
