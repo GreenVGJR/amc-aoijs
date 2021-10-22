2e7b79f63718c7b6e7879ba6e88ae51ef2fce9f3
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:16:30 +0700

    Update slash.js

--- commands/command/slash.js
+++ commands/command/slash.js
@@ -1,6 +1,5 @@
 module.exports = {
   name: "slash",
-  cooldown: "3s",
   code: `$createSlashCommand[$guildID;play;Play song;song:Support YouTube & Soundcloud:true:3]
 $createSlashCommand[$guildID;filter;For list, just leave blank;filter:Use FIlter:false:3]
 $createSlashCommand[$guildID;resume;Resume Song]
@@ -9,7 +8,7 @@ $createSlashCommand[$guildID;stop;Stop Song]
 $title[Successfully created]
 $description[You can use slash command now.] $color[$getVar[color]]
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
-$cooldown[$commandInfo[slash;cooldown];Please wait **%time%** before using again.]
+$cooldown[3s;Please wait **%time%** before using again.]
 $onlyPerms[manageserver;You didnt have permission **Manage Server**.]
 $suppressErrors[failed.]`
 }
