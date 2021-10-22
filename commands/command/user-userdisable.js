2240508344fa64714755887d198374bf8d116bff
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:19:57 +0700

    Update user-userdisable.js

--- commands/command/user-userdisable.js
+++ commands/command/user-userdisable.js
@@ -1,6 +1,5 @@
 module.exports = [{
   name: "user",
-  cooldown: "3s",
   code: `$setServerVar[userid;$mentioned[1]]
 $description[\`$userTag[$mentioned[1]]\` just only can execute command now.]
 $addTimestamp
@@ -9,18 +8,17 @@ $addCmdReactions[✅]
 $onlyIf[$isBot[$mentioned[1]]!=true;Failed.]
 $onlyIf[$mentioned[1]!=;Failed.]
 $argsCheck[1;Mention someone]
-$cooldown[$commandInfo[user;cooldown];Please wait **%time%** before using again.]
+$cooldown[3s;Please wait **%time%** before using again.]
 $onlyPerms[manageserver;Missing Permission, **Manage Server** - User]`
 },
  {
   name: "user-disable",
-  cooldown: "3s",
   code: `$setServerVar[userid;default]
 $description[Change to default.]
 $color[$getVar[color]]
 $addCmdReactions[✅]
 $addTimestamp
 $onlyIf[$getServerVar[userid]!=default;Already default!]
-$cooldown[$commandInfo[user-disable;cooldown];Please wait **%time%** before using again.]
+$cooldown[3s;Please wait **%time%** before using again.]
 $onlyPerms[manageserver;Missing Permission, **Manage Server** - User]`
 }]
