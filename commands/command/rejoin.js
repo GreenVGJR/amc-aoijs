485c62e1abd4a76967f225400ce3b7d556fa307a
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:12:34 +0700

    Update rejoin.js

--- commands/command/rejoin.js
+++ commands/command/rejoin.js
@@ -1,7 +1,6 @@
 module.exports = {
   name: "rejoin",
   aliases: ["reconnect"],
-  cooldown: "3s",
   code: `$if[$messageExists[$channelID;$getUserVar[reactmessageid;$clientID]]==true]
 $deleteMessage[$getUserVar[reactmessageid;$clientID]]
 $endif
@@ -13,7 +12,7 @@ $wait[$multi[$botPing;2]]
 $leaveVC
 $setServerVar[filters;none]
 $endif
-$cooldown[$commandInfo[rejoin;cooldown];Please wait **%time%** before using again.]
+$cooldown[3s;Please wait **%time%** before using again.]
 $onlyIf[$replaceText[$replaceText[$checkCondition[$getServerVar[userid]==default];true;$authorID];false;$getServerVar[userid]]==$authorID;{title:❌ You cant use this command} {color:$getVar[color]}]
 $onlyIf[$voiceID!=;$getVar[errorjoin]]
 $suppressErrors[something just happened.]`
