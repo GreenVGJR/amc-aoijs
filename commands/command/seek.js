01878aa998aa9b311c0718b043251ba52d7d9fd2
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:14:07 +0700

    Update seek.js

--- commands/command/seek.js
+++ commands/command/seek.js
@@ -1,6 +1,5 @@
 module.exports = {
   name: "seek",
-  cooldown: "3s",
   code: `$if[$toLowercase[$message[2]]==save]
 $addField[Seek to \`$replaceText[$replaceText[$checkCondition[$humanizeMS[$multi[$message[1];1000];10]!=];false;0 second];true;$humanizeMS[$multi[$message[1];1000];10]]\`;one-time use saving seek.]
 $footer[Value: $message[1]]
@@ -21,7 +20,7 @@ $onlyIf[$isNumber[$message[1]]!=false;Must number!]
 $argsCheck[>1;Usage: \`seek (number)\`]
 $onlyIf[$songInfo[duration]!=0 Seconds (00:00:00);\`This track was LIVE\`]
 $onlyIf[$queueLength!=0;$getVar[errorqueue]]
-$cooldown[$commandInfo[seek;cooldown];Please wait **%time%** before using again.]
+$cooldown[3s;Please wait **%time%** before using again.]
 $onlyIf[$replaceText[$replaceText[$checkCondition[$getServerVar[userid]==default];true;$authorID];false;$getServerVar[userid]]==$authorID;{title:❌ You cant use this command} {color:$getVar[color]}]
 $onlyIf[$voiceID!=;$getVar[errorjoin]]
 $suppressErrors`
