2f99cb0f71ec5f5beba19fc946b372d8d0daa0c6
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:19:11 +0700

    Update uptime.js

--- commands/command/uptime.js
+++ commands/command/uptime.js
@@ -1,9 +1,8 @@
 module.exports = {
   name: "uptime",
-  cooldown: "3s",
   code: `\`\`\`
 $client[readytimestamp]
 \`\`\`
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
-$cooldown[$commandInfo[uptime;cooldown];Please wait **%time%** before using again.]`
+$cooldown[3s;Please wait **%time%** before using again.]`
 }
