3e750fc24fcdfe0841b391e97d8d2c1f529e2719
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2021-10-22 13:05:49 +0700

    Update playlist-add.js

--- commands/command/playlist-add.js
+++ commands/command/playlist-add.js
@@ -1,6 +1,5 @@
 module.exports = {
 name: "playlist-add",
-cooldown: "3s",
 code: `$setGlobalUserVar[$message[1];$messageSlice[1]]
 $title[Your song has added on $message[1]]
 $footer[Status: $replaceText[$replaceText[$checkCondition[$getGlobalUserVar[$message[1]]==];true;Add];false;Replace]]
@@ -8,7 +7,7 @@ $color[$getVar[color]]
 $setGlobalUserVar[commanduserused;$sum[$getGlobalUserVar[commanduserused];1]]
 $onlyIf[$replaceText[$message[1];-;]<=10;Only available \`10\` slot.]
 $onlyIf[$isNumber[$message[1]]!=false;Must number!]
-$cooldown[$commandInfo[playlist-add;cooldown];Please wait **%time%** before using again.]
+$cooldown[3s;Please wait **%time%** before using again.]
 $argsCheck[>2;Usage: \`playlist-add (number playlist) (song)\`]
 $suppressErrors[something just happened.]`
 }
