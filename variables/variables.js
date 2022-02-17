19d108f2816a671fc69000fa1ebc48db4fa3de58
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-02-17 19:30:49 +0700

    Create variables.js

---
+++ variables/variables.js
@@ -0,0 +1,49 @@
+module.exports = {
+  file: "server.js", //For reboot and stats
+  database: "./database/main/main_scheme_1.sql", //For stats
+
+  //Customize Property For Message
+  pause: "⏸️ Paused.",
+  resume: "▶️ Resumed!",
+  skip: "⏩ Skipped!",  //Available {song}
+  stop: "⏹ Stopped.",  
+  remove: "Removed song on {d-amount}.", //Available {d-amount}
+  filterapply: "Applyed.",
+
+  shuffle: "Shuffle Queue.",
+  errorjoin: "\`❌ You're not in a voice channel.\`",
+  errorqueue: "\`❌ There no song was playing.\`",
+  errorloop: "Only have {amount} song.", //Available {amount}
+  erroruser: "{newEmbed:{title:❌ You cant use this command} {color:$getVar[color]}}",
+  errorsameuser: "You must same in {voice} to use this command.", //Available {voice}
+  customerror: "Something just happened.", //Custom $suppressErrors
+
+  join: "Joined Voice Channel to the {join}.", //Available {join}
+  dc: "Disconnected.",
+  leftvc: "There no song again on queue.", //Description
+  secondleftvc: "Left VC.", //Footer
+
+  //Changing Other
+  color: "a09fff",
+  channelstatus: "", //Optional (channelid), for send ready message
+  vol: "50", //Default Volume
+  maxvol: "150", //Max Volume
+
+  //Changing Other - Advance
+  permission: "2176183360",
+  userid: "default",
+  logmusic: "0",
+  247: "0", //0 = off | 1 = on stay 2 minutes | 2 = stay 24/7
+  last: "null",
+  linkdownload: "",
+  filters: "none",
+
+  //Emoji
+  customemoji1: "https://cdn.discordapp.com/emojis/852434440668184615.png?size=4096",
+  ytemoji: "https://cdn.discordapp.com/emojis/852432148207108110.png?size=4096",
+  scemoji: "https://cdn.discordapp.com/emojis/852432173758676993.png?size=4096",
+  loademoji: "https://cdn.discordapp.com/emojis/895505960427196426.gif?size=4096",
+ 
+  userused: "0",
+  commanduserused: "0"
+}
