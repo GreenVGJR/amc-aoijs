8bf1659c97c84fed86c437149460e7ac210536b5
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-03-01 23:29:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const voice = new Aoijs.Voice(bot, {
   youtube: {
  fetchAuthor: true
  },
-},true); //Prune
+});
 
 //Events
 voice.onTrackStart()
@@ -37,23 +37,35 @@ voice.trackStartCommand({
  channel: "$channelID",
  $if: "v4",
  code: `$if[$getCurrentDuration==0]
-$author[1;Started Playing;$getVar[customemoji1]] 
+$author[1;Started Playing;$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;$getVar[ytemoji]];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;$getVar[scemoji]];false;$getVar[customemoji1]]]] 
 $title[1;$songInfo[title];$songInfo[url]]
+$addField[1;Filters;\`$getServerVar[filters]\`;no]
+$addField[1;24/7;$replaceText[$replaceText[$getGlobalUserVar[247;$songInfo[user.id]];0;\`❌\`];1;\`✅\`];yes]
 $addField[1;Song;\`$numberSeparator[$queueLength]\`;yes]
 $addfield[1;Create;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;<t:$cropText[$songInfo[createdTimestamp];10]:d>];false;none];yes] 
 $addField[1;Like;\`$numberSeparator[$replaceText[$songInfo[likes];null;0]]\`;yes]
 $addField[1;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;Listened];false;Views];\`$numberSeparator[$songInfo[views]]\`;yes] 
-$addField[1;Artist;\`$songInfo[author]\`;yes] $addField[1;Platform;\`$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;YouTube];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;SoundCloud];false;Audio]]\`;yes]
-$addField[1;Duration;\`$replaceText[$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;$humanizeMS[$songInfo[duration];4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]];00:00:00;LIVE]\`;yes]
+$addField[1;Platform;\`$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;YouTube];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;SoundCloud];false;Audio]]\`;yes]
+$addField[1;Artist;\`$songInfo[author]\`;yes] $addField[1;Duration;\`$replaceText[$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;$humanizeMS[$songInfo[duration];4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]];00:00:00;LIVE]\`;yes]
 $addField[1;Requested By;<@$songInfo[user.id]>;no]
 $addTimestamp[1;$dateStamp] 
 $thumbnail[1;$replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;2048]] 
 $color[1;$getVar[color]]
+$playerConfig[$replaceText[$replaceText[$getGlobalUserVar[247;$songInfo[user.id]];0;yes];1;no];0s]
 $volume[$getGlobalUserVar[vol;$songInfo[user.id]]]
 $else
 $volume[$getGlobalUserVar[vol;$songInfo[user.id]]]
 $endif
 `
+})
+
+voice.onTrackEnd()
+
+voice.trackEndCommand({
+ channel: "$channelID",
+ $if: "v4",
+ code: `$resetServerVar[filters]`
+})
 
 bot.variables(require(`./variables/variables.js`))
 
