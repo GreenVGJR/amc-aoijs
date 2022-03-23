4af53c30291c06f2c51b34dd91720c788b25e848
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-03-23 21:11:55 +0700

    Update allmusic.js

--- commands/allmusic/allmusic.js
+++ commands/allmusic/allmusic.js
@@ -6,7 +6,9 @@ $deleteMessage[$get[id]]
 $sendMessage[{newEmbed:{author:Added to queue:$getVar[customemoji1]} {footer:$queueLength Song} {description:$replaceText[$get[message];Added;;1] Song from Playlist.} {color:$getVar[color]}};no]
 $else
 $if[$queueLength<1]
+$if[$getServerVar[logmusic]==1]
 $deleteMessage[$get[id]]
+$endif
 $editMessage[$get[id];{newEmbed:{title:Started Playing} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}}]
 $else
 $editMessage[$get[id];{newEmbed:{author:Added to queue:$getVar[customemoji1]} {footer:$queueLength Song} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}}]
@@ -101,15 +103,67 @@ $onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voi
 $onlyIf[$voiceID!=;$getVar[errorjoin]]`
 },
  {
+ name: "previous",
+ aliases: ["pr"],
+ $if: "v4",
+ code: `$if[$queueLength<1]
+$deleteMessage[$get[id]]
+$editMessage[$get[id];{newEmbed:{title:Started Playing} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}}]
+$else
+$editMessage[$get[id];{newEmbed:{author:Added to queue:$getVar[customemoji1]} {footer:$queueLength Song} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}}]
+$endif
+$if[$checkContains[$getGlobalUserVar[cacheplay];youtube.com]==true]
+$setGlobalUserVar[cacheplay;]
+$let[message;$playTrack[youtube;$getGlobalUserVar[cacheplay]]]
+$elseif[$checkContains[$getGlobalUserVar[cacheplay];soundcloud.com]==true]
+$setGlobalUserVar[cacheplay;]
+$onlyIf[$get[message]!=Added 0;Track not found]
+$let[message;$playTrack[soundcloud;$getGlobalUserVar[cacheplay]]]
+$endelseif
+$else
+$setGlobalUserVar[cacheplay;]
+$let[message;$playTrack[url;$getGlobalUserVar[cacheplay]]]
+$endif
+$if[$voiceID[$clientID]==]
+$joinVc[$voiceID;no;yes;yes]
+$endif
+$let[id;$sendMessage[{newEmbed:{author:Processing:$getVar[loademoji]} {color:$getVar[color]}};yes]]
+$onlyIf[$getGlobalUserVar[cacheplay]!=;Last previous not found]
+$onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voiceID[$clientID]==];true;$voiceID];false;$voiceID[$clientID]]]==true;$replaceText[$getVar[errorsameuser];{voice};<#$voiceID[$clientID]>]]
+$onlyIf[$voiceID!=;$getVar[errorjoin]]`
+},
+ {
+ name: "listen-info",
+ aliases: ["track-info", "music-info", "song-info", "listen"],
+ code: `$title[1;Listened]
+$addField[1;Global;\`$numberSeparator[$getVar[listenglobal]]\`;yes]
+$addField[1;Server;\`$numberSeparator[$getServerVar[listenserver]]\`;yes]
+$addField[1;User;\`$numberSeparator[$getGlobalUserVar[listenuser]]\`;yes]
+$addTimestamp[1]
+$color[1;$getVar[color]]`
+},
+ {
  name: "loop",
  aliases: ["l"],
  $if: "v4",
  code: `$reply[$messageID;no]
-$if[$checkContains[$toLowercase[$message[1]];song;track;music]==true]
+$if[$checkCondition[$message[1]==]-$loopStatus$suppressErrors==true-none]
+$loopMode[song]
+Loop mode \`song\`
+$elseIf[$checkCondition[$message[1]==]-$loopStatus$suppressErrors==true-song]
+$loopMode[queue]
+Loop mode \`queue\`
+$endelseif
+$elseIf[$checkCondition[$message[1]==]-$loopStatus$suppressErrors==true-queue]
+Disable loop.
+$loopMode[none]
+$endelseif
+$else
+$if[$checkContains[$toLowercase[$message[1]];song;track;music;s]==true]
 $loopMode[song]
 Loop mode \`song\`
 $endif
-$if[$checkContains[$toLowercase[$message[1]];queue]==true]
+$if[$checkContains[$toLowercase[$message[1]];queue;q]==true]
 Loop mode \`queue\`
 $loopMode[queue]
 $endif
@@ -120,6 +174,7 @@ $else
 $if[$message[1]==]
 $loopMode[song]
 Loop mode \`song\`
+$endif
 $endif
 $endif
 $onlyIf[$queueLength!=0;$getVar[errorqueue]]
@@ -133,17 +188,18 @@ $onlyIf[$voiceID!=;$getVar[errorjoin]]`
  code: `$if[$message[1]==]
 $reply[$messageID;no]
 $addField[1;Filters;\`$getServerVar[filters]\`;no]
-$addField[1;Filter;\`bassonly, clarity, echo, flanger, deep, haas, gate, nightcore, phaser, pitch, phone, pulsator, reverb, tempo, tremolo, remove, subboost, vaporwave, vibrato\`;yes]
+$addField[1;Filter;$getVar[listfilters];yes]
 $addTimestamp[1;$dateStamp]
-$footer[1;filter <filter> (value optional)]
+$footer[1;filter <filter> (value optional) / selectmenu]
 $color[1;$getVar[color]]
+$addSelectMenu[1;filter;List Filters;1;1;no;Remove:Remove Filter Applyed:off:no;Bass-only:Apply Bass-only Filter:bassonly:no;Clarity:Apply Clarity Filter:clarity:no;Echo:Apply Echo Filter:echo:no;Flanger:Apply Flanger Filter:flanger:no;Deep:Apply Haas Filter:haas:no;Gate:Apply Gate Filter:gate:no;Nightcore:Apply Nightcore Filter:nightcore:no;Phaser:Apply Phaser Filter:phaser:no;Phone:Apply Phone Filter:phone:no;Pulsator:Apply Pulsator Filter:pulsator:no;Reverb:Apply Reverb Filter:reverb:no;Tremolo:Apply Tremolo Filter:tremolo:no;Subboost:Apply Subboost Filter:subboost:no;Vaporwave:Apply Vaporwave Filter:vaporwave:no;Vibrato:Apply Vibrato Filter:vibrato:no]
 $elseIf[$toLowercase[$message[1]]==nightcore]
 $setServerVar[filters;Nightcore]
 $let[filter;$setFilter[{"atempo": "0.720", "asetrate": "48000*1.3"}]]
 $sendMessage[Applyed \`nightcore\`.;no]
 $endelseif
 $elseIf[$checkContains[$toLowercase[$message[1]];remove;clear;reset;off]==true]
-$resetServerVar[filters]
+$setServerVar[filters;$getVar[filters]]
 $let[filter;$resetFilters]
 $sendMessage[Reseted filters.;no]
 $endelseif
@@ -242,6 +298,31 @@ $onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voi
 $onlyIf[$voiceID!=;$getVar[errorjoin]]`
 },
  {
+ name: "filter",
+ type: "interaction",
+ prototype: "selectMenu",
+ code: `$if[$interactionData[values[0]]==nightcore;{execute:filter-nightcore};]
+$if[$checkContains[$interactionData[values[0]];remove;clear;reset;off]==true;{execute:filter-remove};]
+$if[$interactionData[values[0]]==deep;{execute:filter-deep};]
+$if[$interactionData[values[0]]==bassonly;{execute:filter-bassonly};]
+$if[$interactionData[values[0]]==phone;{execute:filter-phone};]
+$if[$interactionData[values[0]]==vibrato;{execute:filter-vibrato};]
+$if[$interactionData[values[0]]==flanger;{execute:filter-flanger};]
+$if[$interactionData[values[0]]==echo;{execute:filter-echo};]
+$if[$interactionData[values[0]]==reverb;{execute:filter-reverb};]
+$if[$interactionData[values[0]]==tremolo;{execute:filter-tremolo};]
+$if[$interactionData[values[0]]==haas;{execute:filter-haas};]
+$if[$interactionData[values[0]]==subboost;{execute:filter-subboost};]
+$if[$interactionData[values[0]]==clarity;{execute:filter-clarity};]
+$if[$interactionData[values[0]]==vaporwave;{execute:filter-vaporwave};]
+$if[$interactionData[values[0]]==phaser;{execute:filter-phaser};]
+$if[$interactionData[values[0]]==pulsator;{execute:filter-pulsator};]
+$if[$interactionData[values[0]]==gate;{execute:filter-gate};]
+$onlyIf[$checkCondition[$voiceID[$interactionData[author.id]]==$replaceText[$replaceText[$checkCondition[$voiceID[$clientID]==];true;$voiceID[$interactionData[author.id]]];false;$voiceID[$clientID]]]==true;]
+$onlyIf[$queueLength!=0;]
+$onlyIf[$voiceID[$interactionData[author.id]]!=;]`
+},
+ {
  name: "queue",
  aliases: ["q"],
  $if: "v4",
@@ -284,7 +365,12 @@ $onlyIf[$voiceID!=;$getVar[errorjoin]]
  code: `$reply[$messageID;no]
 $if[$checkContains[$noMentionMessage[1];:]==true]
 $if[$advancedTextSplit[$noMentionMessage[1];:;3]==]
-$seekTo[$filterMessage[$get[number];-]]
+$if[$getServerVar[filters]!=none]
+$setServerVar[filters;$getVar[filters]]
+$let[filter;$resetFilters]
+$sendMessage[{newEmbed:{description:Due ffmpeg not stable, filter will remove before seek music.} {color:$getVar[color]} {timestamp} {delete:3s}};no]
+$endif
+$seekTo[$multi[$filterMessage[$get[number];-];$getVar[multiseek]]]
 $description[1;Seek to \`$djsEval[new Date($multi[$filterMessage[$get[number];-];1000]).toISOString().substr(11, 8);yes]\`]
 $color[1;$getVar[color]]
 $onlyIf[$checkCondition[$multi[$get[number];1000]>=$songInfo[duration]]!=true;You cant seek more \`$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]\`]
@@ -294,7 +380,12 @@ $onlyIf[$filterMessage[$advancedTextSplit[$noMentionMessage[1];:;1];-]<=59;Max. 
 $onlyIf[$isNumber[$advancedTextSplit[$noMentionMessage[1];:;2]]!=false;Must number]
 $onlyIf[$isNumber[$advancedTextSplit[$noMentionMessage[1];:;1]]!=false;Must number]
 $else
-$seekTo[$filterMessage[$get[number];-]]
+$if[$getServerVar[filters]!=none]
+$setServerVar[filters;$getVar[filters]]
+$let[filter;$resetFilters]
+$sendMessage[{newEmbed:{description:Due ffmpeg not stable, filter will remove before seek music.} {color:$getVar[color]} {timestamp} {delete:3s}};no]
+$endif
+$seekTo[$multi[$filterMessage[$get[number];-];$getVar[multiseek]]]
 $description[1;Seek to \`$djsEval[new Date($multi[$filterMessage[$get[number];-];1000]).toISOString().substr(11, 8);yes]\`]
 $color[1;$getVar[color]]
 $onlyIf[$checkCondition[$multi[$get[number];1000]>=$songInfo[duration]]!=true;You cant seek more \`$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]\`]
@@ -307,7 +398,12 @@ $onlyIf[$isNumber[$advancedTextSplit[$noMentionMessage[1];:;2]]!=false;Must numb
 $onlyIf[$isNumber[$advancedTextSplit[$noMentionMessage[1];:;1]]!=false;Must number]
 $endif
 $else
-$seekTo[$filterMessage[$noMentionMessage[1];-]]
+$seekTo[$multi[$filterMessage[$noMentionMessage[1];-];$getVar[multiseek]]]
+$if[$getServerVar[filters]!=none]
+$setServerVar[filters;$getVar[filters]]
+$let[filter;$resetFilters]
+$sendMessage[{newEmbed:{description:Due ffmpeg not stable, filter will remove before seek music.} {color:$getVar[color]} {timestamp} {delete:3s}};no]
+$endif
 $description[1;Seek to \`$djsEval[new Date($multi[$filterMessage[$noMentionMessage[1];-];1000]).toISOString().substr(11, 8);yes]\`]
 $color[1;$getVar[color]]
 $onlyIf[$checkCondition[$multi[$noMentionMessage[1];1000]>=$songInfo[duration]]!=true;You cant seek more \`$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]\`]
@@ -328,8 +424,9 @@ $title[1;$songInfo[title];$songInfo[url]]
 $thumbnail[1;$replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;2048]]]
 $addField[1;Filters;\`$getServerVar[filters]\`;no]
 $addField[1;URL;[$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;YouTube];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;SoundCloud];false;Audio]]]($songInfo[url] "$songInfo[url]") [- Thumbnail]($replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;2048]] "$replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;2048]]");yes]
-$addField[1;Volume;\`$volume%\`;yes]
 $addField[1;24/7;$replaceText[$replaceText[$getGlobalUserVar[247];0;\`❌\`];1;\`✅\`];yes]
+$addField[1;Loop;\`$replaceText[$replaceText[$checkCondition[$loopStatus==none];true;off];false;on - $loopStatus]\`;yes]
+$addField[1;Volume;\`$volume%\`;yes]
 $addField[1;Song;\`$numberSeparator[$queueLength]\`;yes]
 $addField[1;Duration Left;\`$replaceText[$replaceText[$checkCondition[$songInfo[duration]==0];true;LIVE];false;$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;$humanizeMS[$sub[$songInfo[duration];$getCurrentDuration];4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;0];true;$sub[$songInfo[duration];$getCurrentDuration]]).toISOString().substr(11, 8);yes]]]\`;yes]
 $addField[1;Duration Now;\`$replaceText[$replaceText[$checkCondition[$songInfo[duration]==0];true;LIVE];false;$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;$humanizeMS[$getCurrentDuration;4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com];false;0];true;$getCurrentDuration]).toISOString().substr(11, 8);yes]]]\`;yes]
@@ -353,11 +450,18 @@ $getBotInvite&permissions=$getVar[permission]`
 },
  {
  name: "ping",
+ $if: "v4",
  code: `$reply[$messageID;no]
-$editMessage[$get[id];{newEmbed:{color:$getVar[color]} {timestamp} {footer:$userTag} {field:Websocket Ping:\`$numberSeparator[$get[ping]]ms\`:yes} {field:API Ping:\`$numberSeparator[$get[messageping]]ms\`:yes} {field:Database Ping:\`$numberSeparator[$get[dbping]]ms\`:yes} {field:Message Ping:\`$numberSeparator[$sub[$dateStamp;$get[secondping]]]ms\`:yes}}]
-$let[id;$sendMessage[{newEmbed:{color:$getVar[color]} {timestamp} {footer:$userTag} {field:Websocket Ping:\`Checking\`:yes} {field:API Ping:\`Checking\`:yes} {field:Database Ping:\`Checking\`:yes} {field:Message Ping:\`Checking\`:yes}};yes]]
+$editMessage[$get[id];{newEmbed:{color:$getVar[color]} {timestamp} {footer:$userTag} {field:Websocket Ping:\`$numberSeparator[$get[ping]]ms\`:yes} {field:API Ping:\`$numberSeparator[$get[messageping]]ms\`:yes} {field:Database Ping:\`$numberSeparator[$get[dbping]]ms\`:yes} {field:Voice Ping:\`$numberSeparator[$get[voiceping]]ms\`:yes} {field:Shard Ping:\`$numberSeparator[$get[shardping]]ms\`:yes} {field:Message Ping:\`$numberSeparator[$sub[$dateStamp;$get[secondping]]]ms\`:no}}]
+$let[id;$sendMessage[{newEmbed:{color:$getVar[color]} {timestamp} {footer:$userTag} {field:Websocket Ping:\`Checking\`:yes} {field:API Ping:\`Checking\`:yes} {field:Database Ping:\`Checking\`:yes} {field:Voice Ping:\`Checking\`:yes} {field:Shard Ping:\`Checking\`:yes} {field:Message Ping:\`Checking\`:no}};yes]]
 $let[secondping;$dateStamp]
+$let[shardping;$shardPing]
 $let[ping;$ping]
+$if[$voiceID[$clientID]==]
+$let[voiceping;0]
+$else
+$let[voiceping;$voicePing]
+$endif
 $let[dbping;$dbPing]
 $let[messageping;$messagePing]`
 },
@@ -377,7 +481,7 @@ $addField[1;API Ping;> $numberSeparator[$messagePing]ms;yes]
 $addField[1;DB Ping;> $numberSeparator[$dbPing]ms;yes]
 $addField[1;WS Ping;> $numberSeparator[$ping]ms;yes]
 $addField[1;Platform;> $djsEval[require ('os').platform();yes] | $djsEval[require ('os').arch;yes];yes]
-$addField[1;Last Online;> <t:$cropText[$sub[$dateStamp;$djsEval[client.uptime;yes]];10]:R>;yes]
+$addField[1;Last Online;> <t:$cropText[$readyTimestamp;10]:R>;yes]
 $addField[1;Uptime;> $uptime;yes]
 $footer[1;Ver. $packageVersion ($nodeVersion);$userAvatar[$authorID;512]]
 $thumbnail[1;$userAvatar[$clientID]]
@@ -397,8 +501,9 @@ Stop           : $replaceText[$replaceText[$checkCondition[$getVar[stop]!=];true
 Shuffle        : $replaceText[$replaceText[$checkCondition[$getVar[shuffle]!=];true;✅];false;❌]
 Join           : $replaceText[$replaceText[$checkCondition[$getVar[join]!=];true;✅];false;❌]
 Disconnect     : $replaceText[$replaceText[$checkCondition[$getVar[dc]!=];true;✅];false;❌]
+Play           : $replaceText[$replaceText[$checkCondition[$getVar[errorjoin]!=];true;✅];false;❌]
 UserID         : $replaceText[$replaceText[$checkCondition[$getServerVar[userid]!=default];true;✅];false;❌]
-Log Music      : $replaceText[$replaceText[$checkContains[$getGlobalUserVar[logmusic];0;2];true;✅];false;❌]
+Log Music      : $replaceText[$replaceText[$checkContains[$getGlobalUserVar[logmusic];1];true;✅];false;❌]
 24/7           : $replaceText[$replaceText[$getGlobalUserVar[247];1;✅];0;❌]
 
 Max Volume     : $getServerVar[maxvol]%
@@ -448,8 +553,8 @@ $addField[1;Guide;\`YouTube/SoundCloud/Spotify\`
 > <prefix>play (sc) <name> | SoundCloud
 \`URL\`
 > <prefix>play <url-music>;no]
-$addField[1;Music;\`24/7, play, pause, resume, nowplaying, skip, shuffle, loop, seek, volume, volume-max, stop, filter, queue, join, disconnect\`;no]
-$addField[1;Basic;\`check, stats, uptime, invite, ping\`;no]
+$addField[1;Music;\`24/7, play, pause, resume, nowplaying, previous, skip, shuffle, loop, seek, volume, volume-max, stop, filter, queue, join, disconnect\`;no]
+$addField[1;Basic;\`check, stats, uptime, invite, ping, log, listen-info\`;no]
 $color[1;$getVar[color]]
 $addTimestamp[1;$dateStamp]`
 },
@@ -551,14 +656,14 @@ $onlyIf[$voiceID!=;$getVar[errorjoin]]`
  $if: "v4",
  code: `$if[$getGlobalUserVar[247]==0]
 $if[$queueLength!=0]
-$playerConfig[no;0s]
+$playerConfig[no;0s;yes]
 $onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voiceID[$clientID]==];true;$voiceID];false;$voiceID[$clientID]]]==true;]
 $endif
 Enabled 24/7.
 $setGlobalUserVar[247;1]
 $else
 $if[$queueLength!=0]
-$playerConfig[yes;0s]
+$playerConfig[yes;0s;yes]
 $onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voiceID[$clientID]==];true;$voiceID];false;$voiceID[$clientID]]]==true;]
 $endif
 Disabled 24/7.
@@ -581,4 +686,19 @@ $onlyIf[$queueLength!=1;$replaceText[$getVar[errorloop];{amount};$queueLength]]
 $onlyIf[$queueLength!=0;$getVar[errorqueue]]
 $onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voiceID[$clientID]==];true;$voiceID];false;$voiceID[$clientID]]]==true;$replaceText[$getVar[errorsameuser];{voice};<#$voiceID[$clientID]>]]
 $onlyIf[$voiceID!=;$getVar[errorjoin]]`
+},
+ {
+ name: "log",
+ aliases: ["logmusic", "log-music"],
+ $if: "v4",
+ code: `$reply[$messageID;no]
+$if[$getServerVar[logmusic]==0]
+$setServerVar[logmusic;1]
+Enabled.
+$else
+$setServerVar[logmusic;0]
+Disabled.
+$endif
+$onlyPerms[manageserver;Missing Permission, **Manage Server** - User]
+`
 }]
