98989f0728100a42424b7a8ee346a262c2146554
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-04-10 11:26:29 +0700

    Create create-slash.js

---
+++ commands/slash/create-slash.js
@@ -0,0 +1,70 @@
+module.exports = {
+ name: "slash",
+ code: `$editMessage[$get[id];{newEmbed:{author:Successful Created.} {color:$getVar[color]} {footer:$numberSeparator[$sub[$dateStamp;$get[time]]]ms} {timestamp}}]
+$createApplicationCommand[$guildID;options;List Options Music;true;slash;
+{
+ "name": "music",
+ "description": "24/7, Log, Volume-Max",
+ "type": "SUB_COMMAND",
+ "options": [{
+    "name": "24-7",
+    "description": "Disable/Enable 24/7",
+    "type": 3,
+    "require": "false",
+    "choices": [{
+     "name": "Disable",
+     "value": "off"
+   },
+   {
+     "name": "Enable",
+     "value": "on"
+   }]
+   },
+   {
+    "name": "log",
+    "description": "Disable/Enable Log",
+    "type": 3,
+    "require": "false",
+    "choices": [{
+     "name": "Disable",
+     "value": "off"
+   },
+   {
+     "name": "Enable",
+     "value": "on"
+   }]
+   },
+   {
+    "name": "volume-max",
+    "description": "Change Volume-Max",
+    "type": 10,
+    "require": "false"
+   }]
+}]
+$createApplicationCommand[$guildID;play;Play song;true;slash;
+[{
+ "name": "name",
+ "description": "Write/Put the Song/Link here",
+ "type": 3,
+ "require": "true"
+},
+{
+ "name": "platform",
+ "description": "You can select the platform you want - YouTube (Default)",
+ "type": 3,
+ "require": "false",
+ "choices": [{
+   "name": "YouTube",
+   "value": "youtube"
+   },
+   {
+    "name": "SoundCloud",
+    "value": "soundcloud"
+   }]
+}]
+]
+$let[time;$dateStamp]
+$let[id;$sendMessage[{newEmbed:{author:Creating Slash:$getVar[loademoji]} {color:$getVar[color]} {timestamp}};yes]]
+$onlyBotPerms[useappcmds;Missing Permission, **Use Application Commands** - Bot]
+$onlyPerms[manageserver;Missing Permission, **Manage Server** - User]`
+}
