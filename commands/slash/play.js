53ecfd683fa98a301bcee1d97870407bc87faed7
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-05-13 21:58:32 +0700

    Update play.js

--- commands/slash/play.js
+++ commands/slash/play.js
@@ -4,16 +4,11 @@ module.exports = {
  prototype: "slash",
  $if: "v4",
  code: `$if[$queueLength<1]
-$interactionReply[;{newEmbed:{title:Started Playing} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}};;;;no]
+$interactionEdit[;{newEmbed:{title:Started Playing} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}};;;;no]
 $else
-$interactionReply[;{newEmbed:{author:Added to queue:$getVar[customemoji1]} {footer:$queueLength Song} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}};;;;no]
+$interactionEdit[;{newEmbed:{author:Added to queue:$getVar[customemoji1]} {footer:$queueLength Song} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}};;;;no]
 $endif
-$onlyIf[$checkCondition[$get[message]==Added 0]==false;{
-"content": "Track not found",
-"options": {
- "interaction": "true"
- }
-}]
+$onlyIf[$checkCondition[$get[message]==Added 0]==false;Track not found]
 $if[$checkContains[$message[1];youtu.be;m.youtube;youtube.com]==true]
 $let[message;$playTrack[youtube;$message[1]]]
 $elseIf[$checkContains[$message[1];soundcloud.com;app.goo.gl]==true]
@@ -23,18 +18,22 @@ $elseIf[$checkContains[$message[1];open.spotify.com/track]==true]
 $if[$message[2]==]
 $if[$toLowercase[$getVar[defaultspotify]]==youtube]
 $let[message;$playTrack[youtube;$advancedTextSplit[$get[url];data: ';2;"og:description" content=";2; ·;1] - $advancedTextSplit[$get[url];data: ';2;"og:title" content=";2;";1]]]
+$interactionEdit[;{newEmbed:{author:Processing:$getVar[loademoji]} {footer:Converting to YouTube} {color:$getVar[color]}};;;;no]
 $let[url;$djsEval[require('axios').get('$message[1]');yes]]
 $elseif[$toLowercase[$getVar[defaultspotify]]==soundcloud]
 $let[message;$playTrack[soundcloud;$advancedTextSplit[$get[url];data: ';2;"og:description" content=";2; ·;1] - $advancedTextSplit[$get[url];data: ';2;"og:title" content=";2;";1]]]
+$interactionEdit[;{newEmbed:{author:Processing:$getVar[loademoji]} {footer:Converting to SoundCloud} {color:$getVar[color]}};;;;no]
 $let[url;$djsEval[require('axios').get('$message[1]');yes]]
 $endelseif
 $endif
 $else
 $if[$message[2]==youtube]
 $let[message;$playTrack[youtube;$advancedTextSplit[$get[url];data: ';2;"og:description" content=";2; ·;1] - $advancedTextSplit[$get[url];data: ';2;"og:title" content=";2;";1]]]
+$interactionEdit[;{newEmbed:{author:Processing:$getVar[loademoji]} {footer:Converting to YouTube} {color:$getVar[color]}};;;;no]
 $let[url;$djsEval[require('axios').get('$message[1]');yes]]
 $elseIf[$message[2]==soundcloud]
 $let[message;$playTrack[soundcloud;$advancedTextSplit[$get[url];data: ';2;"og:description" content=";2; ·;1] - $advancedTextSplit[$get[url];data: ';2;"og:title" content=";2;";1]]]
+$interactionEdit[;{newEmbed:{author:Processing:$getVar[loademoji]} {footer:Converting to SoundCloud} {color:$getVar[color]}};;;;no]
 $let[url;$djsEval[require('axios').get('$message[1]');yes]]
 $endelseif
 $endif
@@ -55,8 +54,9 @@ $endif
 $endif
 $endif
 $if[$checkContains[$message[1];soundcloud.com/discover/sets/;youtube.com/playlist?list=]==true]
-$interactionReply[;{newEmbed:{author:Adding to Queue:$getVar[customemoji2]} {footer:This can take long time.} {color:$getVar[color]}};;;;no]
+$interactionEdit[;{newEmbed:{author:Adding to Queue:$getVar[customemoji2]} {footer:This can take long time.} {color:$getVar[color]}};;;;no]
 $endif
+$interactionReply[;{newEmbed:{author:Processing:$getVar[loademoji]} {color:$getVar[color]}};;;;no]
 $onlyIf[$checkContains[$message[1];open.spotify.com/playlist;open.spotify.com/artist;open.spotify.com/album;open.spotify.com/episode]!=true;{
 "content": "Not support",
 "options": {
