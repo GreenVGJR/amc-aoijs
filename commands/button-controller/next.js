45b9b9794cc7ea21556483386311c11b9870a0f6
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-03 22:54:00 +0700

    Update next.js

--- commands/button-controller/next.js
+++ commands/button-controller/next.js
@@ -8,6 +8,7 @@ $interactionReply[Only have \`1\` song.;;;;;yes]
 $else
 $skip
 $endif
+$loop[1;{};controlmusic]
 $onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
 $onlyIf[$songInfo[user.id]==$interactionData[author.id];]
 $onlyIf[$getServerVar[buttonmusicmessage]!=;]
