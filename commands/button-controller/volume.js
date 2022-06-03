ecef10912794ec2ddd8c2fca9b6086846f163c05
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-03 22:06:11 +0700

    Create volume.js

---
+++ commands/button-controller/volume.js
@@ -0,0 +1,44 @@
+module.exports = [{
+ name: "volmute",
+ type: "interaction",
+ prototype: "button",
+ $if: "v4",
+ code: `$loop[1;{};controlmusic]
+$if[$volume$suppressErrors==0]
+$volume[$getGlobalUserVar[vol;$songInfo[user.id]]]
+$else
+$volume[0]
+$endif
+$onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
+$onlyIf[$songInfo[user.id]==$interactionData[author.id];]
+$onlyIf[$getServerVar[buttonmusicmessage]!=;]
+$onlyIf[$queueLength!=0;]`
+},
+ {
+ name: "voldown",
+ type: "interaction",
+ prototype: "button",
+ code: `$loop[1;{};controlmusic]
+$volume[$replaceText[$replaceText[$checkCondition[$get[vol]<=9];true;0];false;$get[vol]]]
+$setGlobalUserVar[vol;$replaceText[$replaceText[$checkCondition[$get[vol]<=9];true;0];false;$get[vol]];$songInfo[user.id]]
+$let[vol;$sub[$volume;10]]
+$onlyIf[$noMentionMessage[1]<=$getServerVar[maxvol];Max. **$getServerVar[maxvol]%**]
+$onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
+$onlyIf[$songInfo[user.id]==$interactionData[author.id];]
+$onlyIf[$getServerVar[buttonmusicmessage]!=;]
+$onlyIf[$queueLength!=0;]`
+},
+ {
+ name: "volup",
+ type: "interaction",
+ prototype: "button",
+ code: `$loop[1;{};controlmusic]
+$volume[$replaceText[$replaceText[$checkCondition[$get[vol]>=$getServerVar[maxvol]];true;$getServerVar[maxvol]];false;$get[vol]]]
+$setGlobalUserVar[vol;$replaceText[$replaceText[$checkCondition[$get[vol]>=$getServerVar[maxvol]];true;$getServerVar[maxvol]];false;$get[vol]];$songInfo[user.id]]
+$let[vol;$sum[$volume;10]]
+$onlyIf[$noMentionMessage[1]<=$getServerVar[maxvol];Max. **$getServerVar[maxvol]%**]
+$onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
+$onlyIf[$songInfo[user.id]==$interactionData[author.id];]
+$onlyIf[$getServerVar[buttonmusicmessage]!=;]
+$onlyIf[$queueLength!=0;]`
+}]
