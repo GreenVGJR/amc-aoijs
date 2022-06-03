54d66ccc774062a951cf852ee02e7e72d22489e4
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-03 22:00:29 +0700

    Update variables.js

--- variables/variables.js
+++ variables/variables.js
@@ -1,6 +1,7 @@
 module.exports = {
-  file: "server.js", //For stats
-  database: "./database/main/main_scheme_1.sql", //For stats
+  //Stats
+  file: "server.js",
+  database: "./database/main/main_scheme_1.sql",
 
   //Customize Property For Message
   pause: "⏸️ Paused.",
@@ -8,8 +9,9 @@ module.exports = {
   skip: "⏩ Skipped!",  //Available {song}
   stop: "⏹ Stopped.",  
   remove: "Removed song on {d-amount}.", //Available {d-amount}
-
   shuffle: "Shuffle Queue.",
+
+  //Error Message
   errorjoin: "\`❌ You're not in a voice channel.\`",
   errorqueue: "\`❌ There no song was playing.\`",
   errorloop: "Only have {amount} song.", //Available {amount}
@@ -18,6 +20,7 @@ module.exports = {
   errorbutton: "You cant use this.",
   customerror: "Something just happened.", //Custom $suppressErrors
 
+  //Event Message
   join: "Joined Voice Channel to the {join}.", //Available {join}
   dc: "Disconnected.",
   leftvc: "There no song again on queue.", //Description
@@ -33,34 +36,40 @@ module.exports = {
   permission: "2176183360",
   deafenclient: "1", //Server Deafen Client, 0 = false | 1 = true
   defaultspotify: "youtube", //YouTube/SoundCloud
-  multiseek: "1000",
+  multiseek: "1000", //Seek Command
   userid: "default",
   logmusic: "1", //0 = off | 1 = on
   247: "0", //0 = off | 1 = stay 24/7
   last: "null",
-  linkdownload: "",
+  linkdownload: "", //Stream HLS (Soon)
   filters: "none",
   cachemessageid: "",
   cacheplay: "",
   ratetime: "0",
   customratetime: "300",
-  buttonmusic: "0", //0 = off | 1 = on
-  buttonmusicmessage: "",
-  buttonmusichannel: "",
   listfilters: "\`bassonly, clarity, deep, echo, flanger, gate, haas, nightcore, phaser, phone, pitch, pulsator, reverb, tempo, tremolo, remove, subboost, vaporwave, vibrato\`",
 
-  //Emoji
+  //Emoji(s)
   customemoji1: "https://cdn.discordapp.com/emojis/852434440668184615.png?size=4096",
   customemoji2: "https://cdn.discordapp.com/emojis/951749233919279125.png?size=4096",
   ytemoji: "https://cdn.discordapp.com/emojis/852432148207108110.png?size=4096",
   scemoji: "https://cdn.discordapp.com/emojis/852432173758676993.png?size=4096",
   loademoji: "https://cdn.discordapp.com/emojis/951749045787959337.gif?size=4096",
  
+  //Listen-Info
   listenuser: "0",
   listenserver: "0",
   listenglobal: "0",
 
-  //Playlist
+  //Music Controller
+  buttonmusic: "0", //0 = off | 1 = on
+  buttonmusicmessage: "",
+  buttonmusichannel: "",
+  timechangemessage: "8000", //0 = disable | milisecond
+  openpublicbutton: "0", //0 = no | 1 = yes (for inside vc users) (soon)
+  forceusebutton: "0", //0 = no | 1 = yes (for everyone, include non-vc users) (soon)
+
+  //Playlist(s)
   playlistuser: "",
   playlistusercount: "1",
   playlistuserpublic: "off",
