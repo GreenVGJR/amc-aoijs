55d6212c688fa0579e85d06c6bfe0acae969f957
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-03 22:05:01 +0700

    Update seek.js

--- commands/button-controller/seek.js
+++ commands/button-controller/seek.js
@@ -2,8 +2,8 @@ module.exports = [{
  name: "downseek",
  type: "interaction",
  prototype: "button",
- code: `$seekTo[$replaceText[$replaceText[$checkCondition[$getCurrentDuration>=10000];false;0];true;$sub[$getCurrentDuration;10000]]]
-$loop[1;{};controlmusic]
+ code: `$loop[1;{};controlmusic]
+$seekTo[$replaceText[$replaceText[$checkCondition[$getCurrentDuration>=10000];false;0];true;$sub[$getCurrentDuration;10000]]]
 $onlyIf[$songInfo[duration]!=0;{
  "content": "This song was \`LIVE\`",
  "ephemeral": "true",
@@ -20,8 +20,8 @@ $onlyIf[$queueLength!=0;]`
  name: "fastseek",
  type: "interaction",
  prototype: "button",
- code: `$seekTo[$sum[$getCurrentDuration;10000]]
-$loop[1;{};controlmusic]
+ code: `$loop[1;{};controlmusic]
+$seekTo[$sum[$getCurrentDuration;10000]]
 $onlyIf[$songInfo[duration]!=0;{
  "content": "This song was \`LIVE\`",
  "ephemeral": "true",
