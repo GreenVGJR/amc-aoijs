28757498c0b79fca5a5a5027b4c4074e160203e3
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-11 11:36:57 +0700

    Update volume.js

--- commands/button-controller/volume.js
+++ commands/button-controller/volume.js
@@ -10,6 +10,7 @@ $else
 $volume[0]
 $endif
 $onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
+$onlyIf[$interactionData[message.id]==$getServerVar[buttonmusicmessage];]
 $onlyIf[$songInfo[user.id]==$interactionData[author.id];]
 $onlyIf[$getServerVar[buttonmusicmessage]!=;]
 $onlyIf[$queueLength!=0;]`
@@ -21,9 +22,10 @@ $onlyIf[$queueLength!=0;]`
  code: `$loop[1;{};controlmusic]
 $volume[$replaceText[$replaceText[$checkCondition[$get[vol]<=9];true;0];false;$get[vol]]]
 $setGlobalUserVar[vol;$replaceText[$replaceText[$checkCondition[$get[vol]<=9];true;0];false;$get[vol]];$songInfo[user.id]]
-$let[vol;$sub[$volume;10]]
+$let[vol;$truncate[$sub[$volume;10]]]
 $onlyIf[$noMentionMessage[1]<=$getServerVar[maxvol];Max. **$getServerVar[maxvol]%**]
 $onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
+$onlyIf[$interactionData[message.id]==$getServerVar[buttonmusicmessage];]
 $onlyIf[$songInfo[user.id]==$interactionData[author.id];]
 $onlyIf[$getServerVar[buttonmusicmessage]!=;]
 $onlyIf[$queueLength!=0;]`
@@ -35,9 +37,10 @@ $onlyIf[$queueLength!=0;]`
  code: `$loop[1;{};controlmusic]
 $volume[$replaceText[$replaceText[$checkCondition[$get[vol]>=$getServerVar[maxvol]];true;$getServerVar[maxvol]];false;$get[vol]]]
 $setGlobalUserVar[vol;$replaceText[$replaceText[$checkCondition[$get[vol]>=$getServerVar[maxvol]];true;$getServerVar[maxvol]];false;$get[vol]];$songInfo[user.id]]
-$let[vol;$sum[$volume;10]]
+$let[vol;$truncate[$sum[$volume;10]]]
 $onlyIf[$noMentionMessage[1]<=$getServerVar[maxvol];Max. **$getServerVar[maxvol]%**]
 $onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
+$onlyIf[$interactionData[message.id]==$getServerVar[buttonmusicmessage];]
 $onlyIf[$songInfo[user.id]==$interactionData[author.id];]
 $onlyIf[$getServerVar[buttonmusicmessage]!=;]
 $onlyIf[$queueLength!=0;]`
