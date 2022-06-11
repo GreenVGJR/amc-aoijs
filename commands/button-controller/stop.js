5d22888ef51a755cd950389e359b3fcf9f109020
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-11 11:36:02 +0700

    Update stop.js

--- commands/button-controller/stop.js
+++ commands/button-controller/stop.js
@@ -7,6 +7,7 @@ $onlyIf[$getGlobalUserVar[247;$interactionData[author.id]]!=1;]
 $stop
 $resetPlayer
 $onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
+$onlyIf[$interactionData[message.id]==$getServerVar[buttonmusicmessage];]
 $onlyIf[$songInfo[user.id]==$interactionData[author.id];]
 $onlyIf[$getServerVar[buttonmusicmessage]!=;]
 $onlyIf[$queueLength!=0;]`
