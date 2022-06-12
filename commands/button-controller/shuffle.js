d68d0d754d1108a17a4595c845901adacae48bb8
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-12 08:57:15 +0700

    Update shuffle.js

--- commands/button-controller/shuffle.js
+++ commands/button-controller/shuffle.js
@@ -4,11 +4,18 @@ module.exports = {
  prototype: "button",
  code: `$interactionReply[;{newEmbed:{author:$getVar[shuffle]:$getVar[customemoji1]}
 {description:**Queue - $numberSeparator[$queueLength] Song**
-$queue[1;5;\`{position} |\` **[{title}]({url})**}
+$queue[1;5;\`{position} |\` **[{title}]({url})**]}
 {color:$getVar[color]}
 {thumbnail:$replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;2048]]}
 {timestamp}};;;;yes]
 $shuffleQueue
+$onlyIf[$queueLength!=1;{
+ "content": "Only have \`1\` song.",
+ "ephemeral": "true",
+ "options": {
+  "interaction": "true"
+ }
+}]
 $onlyIf[$getServerVar[buttonmusic]!=0;{execute:forcemusicoff}]
 $onlyIf[$interactionData[message.id]==$getServerVar[buttonmusicmessage];]
 $onlyIf[$songInfo[user.id]==$interactionData[author.id];]
