4bb09dd6c9eb1f1f60817059154fd2607bd7335d
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-06-22 21:54:44 +0700

    Update play.js

--- commands/slash/play.js
+++ commands/slash/play.js
@@ -3,11 +3,12 @@ module.exports = {
  type: "interaction",
  prototype: "slash",
  $if: "v4",
- code: `$if[$queueLength<1]
-$interactionEdit[;{newEmbed:{title:Started Playing} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}};;;;no]
+ code: `$if[$checkContains[$message[1];soundcloud.com/discover/sets/;youtube.com/playlist?list=;open.spotify.com/playlist]==true]
+$interactionEdit[;{newEmbed:{author:Added to queue:$getVar[customemoji1]} {footer:$queueLength Song} {description:\`$replaceText[$get[message];Added ;;1]\` Song from Playlist.} {color:$getVar[color]}};;;;no]
 $else
-$interactionEdit[;{newEmbed:{author:Added to queue:$getVar[customemoji1]} {footer:$queueLength Song} {description:$replaceText[$get[message];Added;;1]} {color:$getVar[color]}};;;;no]
+$interactionEdit[;{newEmbed:{author:$replaceText[$replaceText[$checkCondition[$queueLength==1];true;Started Playing];false;Added to Queue]:$getVar[customemoji1]} {footer:$replaceText[$replaceText[$checkCondition[$queueLength==1];true;];false;$queueLength Song]} {description:\`$replaceText[$get[message];Added ;;1]\`} {color:$getVar[color]}};;;;no]
 $endif
+$onlyIf[$hasPlayer!=false;]
 $onlyIf[$checkCondition[$get[message]==Added 0]==false;Track not found]
 $if[$checkContains[$message[1];youtu.be;m.youtube;youtube.com]==true]
 $let[message;$playTrack[youtube;$message[1]]]
@@ -39,8 +40,11 @@ $endelseif
 $endif
 $endif
 $endelseif
+$elseIf[$checkContains[$message[1];open.spotify.com/playlist]==true]
+$let[message;$playTrack[spotify;$message[1]]]
+$endelseif
 $else
-$if[$isValidLink[$message[1]]==true]
+$if[$replaceText[$replaceText[$checkContains[$message[1];youtu.be;m.youtube;youtube.com;soundcloud.com;app.goo.gl;soundcloud.com/discover/sets/;youtube.com/playlist?list=;open.spotify.com/playlist;open.spotify.com/track];true;];false;$isValidLink[$message[1]]]==true]
 $let[message;$playTrack[url;$message[1]]]
 $else
 $if[$message[2]==soundcloud]
@@ -48,24 +52,27 @@ $let[message;$playTrack[soundcloud;$message[1]]]
 $elseIf[$message[2]==youtube]
 $let[message;$playTrack[youtube;$message[1]]]
 $endelseif
+$elseIf[$message[1]==]
+$interactionEdit[;{newEmbed:{author:Error} {description:Message not found} {color:$getVar[color]}};;;;no]
+$endelseif
 $else
 $let[message;$playTrack[youtube;$message[1]]]
 $endif
 $endif
 $endif
-$if[$checkContains[$message[1];soundcloud.com/discover/sets/;youtube.com/playlist?list=]==true]
+$if[$checkContains[$message[1];soundcloud.com/discover/sets/;youtube.com/playlist?list=;open.spotify.com/playlist]==true]
 $interactionEdit[;{newEmbed:{author:Adding to Queue:$getVar[customemoji2]} {footer:This can take long time.} {color:$getVar[color]}};;;;no]
 $endif
+$if[$voiceID[$clientID]==]
+$joinVc[$voiceID;no;yes;yes]
+$endif
 $interactionReply[;{newEmbed:{author:Processing:$getVar[loademoji]} {color:$getVar[color]}};;;;no]
-$onlyIf[$checkContains[$message[1];open.spotify.com/playlist;open.spotify.com/artist;open.spotify.com/album;open.spotify.com/episode]!=true;{
+$onlyIf[$checkContains[$message[1];open.spotify.com/artist;open.spotify.com/album;open.spotify.com/episode]!=true;{
 "content": "Not support",
 "options": {
  "interaction": "true"
  }
 }]
-$if[$voiceID[$clientID]==]
-$joinVc[$voiceID;no;yes;yes]
-$endif
 $onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voiceID[$clientID]==];true;$voiceID];false;$voiceID[$clientID]]]==true;{
 "content": "$replaceText[$getVar[errorsameuser];{voice};<#$voiceID[$clientID]>]",
 "ephemeral": "true",
